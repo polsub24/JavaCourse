94a532122de0988aba7c18cf2719987b279ad150
Subham Sarkar <180067022+polsub24@users.noreply.github.com>
2025-03-07 14:26:06 +0530

    Create Ex_Superkeyword.java

---
+++ Module4_Inheritance/Ex_Superkeyword.java
@@ -0,0 +1,35 @@
+class P1{
+	P1(){
+		System.out.println("parent class constructor");
+	}
+	int i =5;
+	void display() {
+		System.out.println("parent class");
+	}
+}
+class C1 extends P1{
+	C1(){
+		System.out.println("child class constructor");
+	}
+	C1(int j){
+		super();
+		System.out.println(j);
+	}
+	int i =10;
+	void display() {
+		System.out.println("Child class");
+	}
+	void show() {
+		display();//method overriding (child class method)
+		super.display(); //parent class method
+		System.out.println(i);
+		System.out.println(super.i); //parent instance variable 
+	}
+}
+public class Ex_Superkeyword {
+	public static void main(String[] args) {
+		 C1 obj = new C1(15);
+		 obj.display();
+		 obj.show();
+	}
+}
