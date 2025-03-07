19de126f69639d280ff0adf740ffe1ea053873fc
Subham Sarkar <180067022+polsub24@users.noreply.github.com>
2025-03-07 14:37:24 +0530

    Create try&catch.java

---
+++ Module5/try&catch.java
@@ -0,0 +1,12 @@
+public class Try {
+	public static void main(String[] args) {
+		try {
+			System.out.println(5/0);
+		}
+		catch (ArithmeticException obj) {
+			System.out.println("arithmetic exception handled");
+			System.out.println(obj);
+		}
+		System.out.println("rest of the code");
+	}
+}
