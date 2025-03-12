1d60ae07ac3d3420492637e130d5bea16fead8c2
Subham Sarkar <180067022+polsub24@users.noreply.github.com>
2025-03-12 14:27:29 +0530

    Update PArent.java

--- pack1/PArent.java
+++ pack1/PArent.java
@@ -1,6 +1,8 @@
 package pack1;
 public class PArent {
-	public void show() {
+	protected void show() {
 		System.out.println("parent");
 	}
+	//public working in Child class
+	// protected keyword is used for exclusivity of use with Child class and only child extended to parent can access it
 }
