8ad9f718df11e99aa7a6c22e78efc4073fcf3fd4
Subham Sarkar <180067022+polsub24@users.noreply.github.com>
2025-03-28 16:55:43 +0530

    Update TestBike.java

--- DA2/TestBike.java
+++ DA2/TestBike.java
@@ -1,1 +1,26 @@
+package da3;
+class Vehicle {
+	void move(){
+		System.out.println("Vehicle moved");
+	}
+}
+class Car extends Vehicle {
+	void move(){
+		super.move();
+		System.out.println("Car moved");
+	}
+}
+class Bike extends Vehicle{
+	void move(){
+		System.out.println("Bike moved");
+	}
+}
+public class TestBike {
+	public static void main(String[] args) {
+		Vehicle obj = new Car();
+		Vehicle obj1 = new Bike();
+		obj.move();
+		obj1.move();
 
+	}
+}
