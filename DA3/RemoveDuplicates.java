a2a4aff0368ec183bd201e4167f6c151f0cf84fe
Subham Sarkar <180067022+polsub24@users.noreply.github.com>
2025-04-01 19:31:29 +0530

    Update RemoveDuplicates.java

--- DA3/RemoveDuplicates.java
+++ DA3/RemoveDuplicates.java
@@ -1,1 +1,23 @@
 
+package da4;
+import java.util.*;
+public class RemoveDuplicates {
+    public static void removeDuplicate(ArrayList<Integer> list) {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>(list);
+        list.clear();
+        list.addAll(set);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("Enter 10 integers: ");
+        for (int i = 0; i < 10; i++) {
+            list.add(scanner.nextInt());
+        }
+        removeDuplicate(list);
+        System.out.print("Distinct integers: ");
+        for (int num : list) {
+            System.out.print(num + " ");
+        }
+    }
+}
