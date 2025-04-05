6544d7c4865969a30ec9dded153ca00dbd2d83af
Subham Sarkar <180067022+polsub24@users.noreply.github.com>
2025-04-05 21:32:28 +0530

    Update Ex8.java

--- DA4/Ex8.java
+++ DA4/Ex8.java
@@ -1,1 +1,30 @@
+package FileHandling;
+import java.io.*;
+public class{
+  public static void main(String[] args){
+    try{
+      File obj = new File("mydata3.txt");
+      FileOutputStream fout = new FileOutputStream(obj);
+      BufferedOutputStream ob = new BufferedOutputStream(fout);
+      ob.write(72);
+      ob.write(73);
+      System.out.println("written successfully");
+      ob.close();
+      fout.close();
 
+      FileInputStream fin = new FileInputStream(obj);
+      BufferedInputStream ob1 = new BufferedInputStream(fin);
+      //int i = ob1.read();
+      int i=0;
+      while((i=ob1.read())!=1){
+        System.out.println((char));
+      }
+      System.out.println("\nreading completed");
+      ob1.close();
+      fin.close();
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
+  }
+}
